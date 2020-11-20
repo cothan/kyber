948ac13d5a0ae6d46058c912da9209c609745d63
cothan <cothannguyen@gmail.com>
2020-11-19 20:25:11 -0500

    add namespace

--- neon/cbd.h
+++ neon/cbd.h
@@ -5,10 +5,10 @@
 #include "params.h"
 #include "poly.h"
 
-#define cbd_eta1 KYBER_NAMESPACE(_cbd_eta1)
+#define cbd_eta1 KYBER_NAMESPACE(_neon_cbd_eta1)
 void cbd_eta1(poly *r, const uint8_t buf[KYBER_ETA1*KYBER_N/4]);
 
-#define cbd_eta2 KYBER_NAMESPACE(_cbd_eta2)
+#define cbd_eta2 KYBER_NAMESPACE(_neon_cbd_eta2)
 void cbd_eta2(poly *r, const uint8_t buf[KYBER_ETA2*KYBER_N/4]);
 
 
