9ed4dd522770089f5393cb471b0d0e934185d29e
cothan <cothannguyen@gmail.com>
2020-11-19 20:00:26 -0500

    clean up & rename

--- neon/poly.h
+++ neon/poly.h
@@ -12,67 +12,54 @@ typedef struct{
   int16_t coeffs[KYBER_N];
 } poly;
 
-#define poly_compress KYBER_NAMESPACE(_poly_compress)
+#define poly_compress KYBER_NAMESPACE(_ref_poly_compress)
 void poly_compress(uint8_t r[KYBER_POLYCOMPRESSEDBYTES], poly *a);
-#define poly_decompress KYBER_NAMESPACE(_poly_decompress)
+#define poly_decompress KYBER_NAMESPACE(_ref_poly_decompress)
 void poly_decompress(poly *r, const uint8_t a[KYBER_POLYCOMPRESSEDBYTES]);
 
-#define poly_tobytes KYBER_NAMESPACE(_poly_tobytes)
-void poly_tobytes(uint8_t r[KYBER_POLYBYTES], poly *a);
-#define poly_frombytes KYBER_NAMESPACE(_poly_frombytes)
+#define poly_tobytes KYBER_NAMESPACE(_ref_poly_tobytes)
+void poly_tobytes(uint8_t r[KYBER_POLYBYTES], poly *a, 
+                  const uint8_t reduce);
+#define poly_frombytes KYBER_NAMESPACE(_ref_poly_frombytes)
 void poly_frombytes(poly *r, const uint8_t a[KYBER_POLYBYTES]);
 
-#define poly_frommsg KYBER_NAMESPACE(_poly_frommsg)
+#define poly_frommsg KYBER_NAMESPACE(_ref_poly_frommsg)
 void poly_frommsg(poly *r, const uint8_t msg[KYBER_INDCPA_MSGBYTES]);
-#define poly_tomsg KYBER_NAMESPACE(_poly_tomsg)
+#define poly_tomsg KYBER_NAMESPACE(_ref_poly_tomsg)
 void poly_tomsg(uint8_t msg[KYBER_INDCPA_MSGBYTES], poly *r);
-
-#define poly_getnoise_eta1 KYBER_NAMESPACE(_poly_getnoise_eta1)
-void poly_getnoise_eta1(poly *r, const uint8_t seed[KYBER_SYMBYTES], uint8_t nonce);
-
-#define poly_getnoise_eta2 KYBER_NAMESPACE(_poly_getnoise_eta2)
-void poly_getnoise_eta2(poly *r, const uint8_t seed[KYBER_SYMBYTES], uint8_t nonce);
-
-#define poly_ntt KYBER_NAMESPACE(_poly_ntt)
-void poly_ntt(poly *r);
-#define poly_invntt_tomont KYBER_NAMESPACE(_poly_invntt_tomont)
-void poly_invntt_tomont(poly *r);
-#define poly_basemul_montgomery KYBER_NAMESPACE(_poly_basemul_montgomery)
-void poly_basemul_montgomery(poly *r, const poly *a, const poly *b);
-#define poly_tomont KYBER_NAMESPACE(_poly_tomont)
-void poly_tomont(poly *r);
-
-#define poly_reduce KYBER_NAMESPACE(_poly_reduce)
-void poly_reduce(poly *r);
-#define poly_csubq KYBER_NAMESPACE(_poly_csubq)
-void poly_csubq(poly *r);
-
-#define poly_add KYBER_NAMESPACE(_poly_add)
-void poly_add(poly *r, const poly *a, const poly *b);
-#define poly_sub KYBER_NAMESPACE(_poly_sub)
-void poly_sub(poly *r, const poly *a, const poly *b);
 
 // NEON
 
 #define neon_poly_reduce KYBER_NAMESPACE(_neon_poly_reduce)
 void neon_poly_reduce(poly *c);
-#define neon_poly_add_reduce KYBER_NAMESPACE(_neon_poly_add_reduce)
-void neon_poly_add_reduce(poly *c, const poly *a);
-#define neon_poly_sub_reduce KYBER_NAMESPACE(_neon_poly_sub_reduce)
-void neon_poly_sub_reduce(poly *c, const poly *a);
-#define neon_poly_add_add_reduce KYBER_NAMESPACE(_neon_poly_add_add_reduce)
-void neon_poly_add_add_reduce(poly *c, const poly *a, const poly *b);
+#define neon_poly_add_reduce_csubq KYBER_NAMESPACE(_neon_poly_add_reduce_csubq)
+void neon_poly_add_reduce_csubq(poly *c, const poly *a);
 
+#define neon_poly_sub_reduce_csubq KYBER_NAMESPACE(_neon_poly_sub_reduce_csubq)
+void neon_poly_sub_reduce_csubq(poly *c, const poly *a);
+
+#define neon_poly_add_add_reduce_csubq KYBER_NAMESPACE(_neon_poly_add_add_reduce_csubq)
+void neon_poly_add_add_reduce_csubq(poly *c, const poly *a, const poly *b);
+
+#define neon_poly_getnoise_eta1_2x KYBER_NAMESPACE(_neon_poly_getnoise_eta1_2x)
 void neon_poly_getnoise_eta1_2x(poly *vec1, poly *vec2,
                                 const uint8_t seed[KYBER_SYMBYTES],
                                 uint8_t nonce1, uint8_t nonce2);
 
-
+#define neon_poly_getnoise_eta2_2x KYBER_NAMESPACE(_neon_poly_getnoise_eta2_2x)
 void neon_poly_getnoise_eta2_2x(poly *vec1, poly *vec2,
                                 const uint8_t seed[KYBER_SYMBYTES],
                                 uint8_t nonce1, uint8_t nonce2);
 
+#define neon_poly_getnoise_eta2 KYBER_NAMESPACE(_neon_poly_getnoise_eta2)
 void neon_poly_getnoise_eta2(poly *r,
                              const uint8_t seed[KYBER_SYMBYTES],
                              uint8_t nonce);
+
+#define poly_csubq KYBER_NAMESPACE(_poly_csubq)
+void poly_csubq(poly *r);
+
 #endif
+
+
+  
