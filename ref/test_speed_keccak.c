a020ab99ab52c6c4c0a1c9e0c1d587525dd98175
cothan <cothannguyen@gmail.com>
2021-01-06 01:55:27 -0500

    update to region, the cycles is incorrect

--- ref/test_speed_keccak.c
+++ ref/test_speed_keccak.c
@@ -16,122 +16,76 @@
 
 #define NTESTS 1000000
 
-// uint64_t t[NTESTS];
 uint8_t seed[KYBER_SYMBYTES] = {0};
 
 int main()
 {
     unsigned int i;
-    // unsigned char pk[CRYPTO_PUBLICKEYBYTES] = {0};
-    // unsigned char sk[CRYPTO_SECRETKEYBYTES] = {0};
-    // unsigned char ct[CRYPTO_CIPHERTEXTBYTES] = {0};
-    // unsigned char key[CRYPTO_BYTES] = {0};
-    // unsigned char kexsenda[KEX_AKE_SENDABYTES] = {0};
-    // unsigned char kexsendb[KEX_AKE_SENDBBYTES] = {0};
-    // unsigned char kexkey[KEX_SSBYTES] = {0};
-    // unsigned char msg[KYBER_INDCPA_MSGBYTES] = {0};
     uint8_t buf1eta2[KYBER_ETA2 * KYBER_N / 4],
-            buf2eta2[KYBER_ETA2 * KYBER_N / 4];
+        buf2eta2[KYBER_ETA2 * KYBER_N / 4];
     uint8_t buf1eta1[KYBER_ETA1 * KYBER_N / 4],
-            buf2eta1[KYBER_ETA1 * KYBER_N / 4];
+        buf2eta1[KYBER_ETA1 * KYBER_N / 4];
     polyvec matrix[KYBER_K];
     poly ap, bp;
-    int retval; 
+    int retval;
     xof_state state1, state2;
 
-    #define GEN_MATRIX_NBLOCKS ((12*KYBER_N/8*(1 << 12)/KYBER_Q \
-                             + XOF_BLOCKBYTES)/XOF_BLOCKBYTES)
+#define GEN_MATRIX_NBLOCKS ((12 * KYBER_N / 8 * (1 << 12) / KYBER_Q + XOF_BLOCKBYTES) / XOF_BLOCKBYTES)
 
     uint8_t buf0[GEN_MATRIX_NBLOCKS * XOF_BLOCKBYTES + 2],
-            buf1[GEN_MATRIX_NBLOCKS * XOF_BLOCKBYTES + 2];
+        buf1[GEN_MATRIX_NBLOCKS * XOF_BLOCKBYTES + 2];
 
     printf("NTESTS: %d\n", NTESTS);
     long_long start, end;
 
-    //start = cpucycles();
+    PAPI_hl_region_begin("gen_matrix");
     for (i = 0; i < NTESTS; i++)
     {
-        // t[i] = cpucycles();
-        PAPI_hl_region_begin("gen_matrix");  
         gen_matrix(matrix, seed, 0);
-        PAPI_hl_region_end("gen_matrix");
-        
     }
-    //end = cpucycles() - start;
-    //printf("gen_a: %lf\n", (double) end/NTESTS);
+    PAPI_hl_region_end("gen_matrix");
 
-    //start = cpucycles();
+    PAPI_hl_region_begin("eta1");
     for (i = 0; i < NTESTS; i++)
     {
-        // t[i] = cpucycles();
-        PAPI_hl_region_begin("eta1");  
-        
         poly_getnoise_eta1(&ap, seed, 1);
         poly_getnoise_eta1(&bp, seed, 0);
-      
-        PAPI_hl_region_end("eta1");
     }
-    //end = cpucycles() - start;
-    //printf("poly_getnoise_eta1: %lf\n", (double) end/NTESTS);
+    PAPI_hl_region_end("eta1");
 
-    //start = cpucycles();
+    PAPI_hl_region_begin("eta2");
     for (i = 0; i < NTESTS; i++)
     {
-        // t[i] = cpucycles();
-        PAPI_hl_region_begin("eta2");  
-        
         poly_getnoise_eta2(&ap, seed, 0);
         poly_getnoise_eta2(&bp, seed, 1);
-        PAPI_hl_region_end("eta2");
     }
-    //end = cpucycles() - start;
-    //printf("poly_getnoise_eta2: %lf\n", (double) end/NTESTS);
+    PAPI_hl_region_end("eta2");
 
-    //start = cpucycles();
+    PAPI_hl_region_begin("prf");
     for (i = 0; i < NTESTS; i++)
     {
-        // t[i] = cpucycles();
-        PAPI_hl_region_begin("prf");  
-        
         prf(buf1eta1, sizeof(buf1eta1), seed, 0);
         prf(buf2eta1, sizeof(buf2eta1), seed, 0);
-        
-        PAPI_hl_region_end("prf");
     }
-    //end = cpucycles() - start;
-    //printf("prf: %d->%d: %lf\n", 32,  sizeof(buf1eta1), (double) end/NTESTS);
+    PAPI_hl_region_end("prf");
 
-    //start = cpucycles();
+    PAPI_hl_region_begin("prf2");
     for (i = 0; i < NTESTS; i++)
     {
-        // t[i] = cpucycles();
-        PAPI_hl_region_begin("prf2");  
-        
         prf(buf1eta2, sizeof(buf1eta2), seed, 0);
         prf(buf2eta2, sizeof(buf2eta2), seed, 0);
-        
-        PAPI_hl_region_end("prf2");
     }
-    //end = cpucycles() - start;
-    //printf("prf: %d->%d: %lf\n",32, sizeof(buf2eta2), (double) end/NTESTS);
+    PAPI_hl_region_end("prf2");
 
-
-    // start = cpucycles();
+    PAPI_hl_region_begin("abs");
     for (i = 0; i < NTESTS; i++)
     {
-        // t[i] = cpucycles();
-        PAPI_hl_region_begin("abs");  
-        
         kyber_shake128_absorb(&state1, seed, 0, 1);
         kyber_shake128_absorb(&state2, seed, 0, 1);
         xof_squeezeblocks(buf0, GEN_MATRIX_NBLOCKS, &state1);
         xof_squeezeblocks(buf1, GEN_MATRIX_NBLOCKS, &state2);
-        
-        PAPI_hl_region_end("abs");
     }
-    //end = cpucycles() - start;
-    //printf("SHAKE128 ABS SQZ: %d->%d: %lf\n", 32, sizeof(buf1), (double) end/NTESTS);
-
+    PAPI_hl_region_end("abs");
 
     return 0;
 }
