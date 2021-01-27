7606d9559df899ad77cdfee996dcd4062a074e9a
cothan <cothannguyen@gmail.com>
2021-01-27 00:25:10 -0500

    improve memory

--- neon/ntt.h
+++ neon/ntt.h
@@ -5,16 +5,16 @@
 #include "params.h"
 
 #define zetas KYBER_NAMESPACE(_ref_zetas)
-extern const int16_t zetas[128];
+extern const int16_t zetas[64];
 
-#define zetas_inv KYBER_NAMESPACE(_ref_zetas_inv)
-extern const int16_t zetas_inv[128];
+// #define zetas_inv KYBER_NAMESPACE(_ref_zetas_inv)
+// extern const int16_t zetas_inv[128];
 
 #define neon_zetas_inv KYBER_NAMESPACE(_neon_zetas_inv)
-extern const int16_t neon_zetas_inv[384];
+extern const int16_t neon_zetas_inv[280];
 
 #define neon_zetas KYBER_NAMESPACE(_neon_zetas)
-extern const int16_t neon_zetas[440];
+extern const int16_t neon_zetas[224];
 
 #define neon_ntt KYBER_NAMESPACE(_neon_ntt)
 void neon_ntt(int16_t poly[256]);
