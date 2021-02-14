074a43f856f0d7676dbf390441d4f27cad3167b8
cothan <cothannguyen@gmail.com>
2021-02-13 20:58:35 -0500

    update benchmark

--- ref/test_speed_region.c
+++ ref/test_speed_region.c
@@ -2,14 +2,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "api.h"
+#include "kem.h"
 #include "kex.h"
 #include "params.h"
 #include "indcpa.h"
 #include "poly.h"
 #include "polyvec.h"
-#include "cpucycles.h"
-#include "speed_print.h"
+#include "papi.h"
 #include "ntt.h"
 
 #define NTESTS 100000
@@ -24,12 +23,13 @@ int main()
   unsigned char sk[CRYPTO_SECRETKEYBYTES] = {0};
   unsigned char ct[CRYPTO_CIPHERTEXTBYTES] = {0};
   unsigned char key[CRYPTO_BYTES] = {0};
-  unsigned char kexsenda[KEX_AKE_SENDABYTES] = {0};
-  unsigned char kexsendb[KEX_AKE_SENDBBYTES] = {0};
-  unsigned char kexkey[KEX_SSBYTES] = {0};
+  // unsigned char kexsenda[KEX_AKE_SENDABYTES] = {0};
+  // unsigned char kexsendb[KEX_AKE_SENDBBYTES] = {0};
+  // unsigned char kexkey[KEX_SSBYTES] = {0};
   unsigned char msg[KYBER_INDCPA_MSGBYTES] = {0};
   polyvec matrix[KYBER_K];
-  poly ap, bp;
+  poly ap;
+  // poly bp;
 
   PAPI_hl_region_begin("gen_matrix");
   for(i=0;i<NTESTS;i++) {
@@ -37,17 +37,17 @@ int main()
   }
   PAPI_hl_region_end("gen_matrix");
 
-  PAPI_hl_region_begin("neon_poly_getnoise_eta1_2x");
+  PAPI_hl_region_begin("poly_getnoise_eta1");
   for(i=0;i<NTESTS;i++) {
     poly_getnoise_eta1(&ap, seed, 1);
   }
-  PAPI_hl_region_end("neon_poly_getnoise_eta1_2x");
+  PAPI_hl_region_end("poly_getnoise_eta1");
 
-  PAPI_hl_region_begin("neon_poly_getnoise_eta2");
+  PAPI_hl_region_begin("poly_getnoise_eta2");
   for(i=0;i<NTESTS;i++) {
     poly_getnoise_eta2(&ap, seed, 0);
   }
-  PAPI_hl_region_end("neon_poly_getnoise_eta2");
+  PAPI_hl_region_end("poly_getnoise_eta2");
 
   PAPI_hl_region_begin("poly_tomsg");
   for(i=0;i<NTESTS;i++) {
@@ -62,17 +62,17 @@ int main()
   PAPI_hl_region_end("poly_frommsg");
 
 
-  PAPI_hl_region_begin("neon_ntt");
+  PAPI_hl_region_begin("ref_ntt");
   for(i=0;i<NTESTS;i++) {
     ntt(ap.coeffs);
   }
-  PAPI_hl_region_end("neon_ntt");
+  PAPI_hl_region_end("ref_ntt");
 
-  PAPI_hl_region_begin("neon_invntt");
+  PAPI_hl_region_begin("ref_invntt");
   for(i=0;i<NTESTS;i++) {
     invntt(ap.coeffs);
   }
-  PAPI_hl_region_end("neon_invntt");
+  PAPI_hl_region_end("ref_invntt");
 
   PAPI_hl_region_begin("crypto_kem_keypair");
   for(i=0;i<NTESTS;i++) {
