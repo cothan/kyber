eeb9014732726d59ad383d58b2ff283fa7597254
cothan <cothannguyen@gmail.com>
2021-02-14 23:01:35 -0500

    rename, restructure

--- neon/neon_polyvec.c
+++ neon/neon_polyvec.c
@@ -1,13 +1,12 @@
 #include <arm_neon.h>
 #include "params.h"
-#include "reduce.h"
-#include "ntt.h"
+#include "poly.h"
 #include "polyvec.h"
 
 #define _V (((1U << 26) + KYBER_Q / 2) / KYBER_Q)
 
 /*************************************************
-* Name:        polyvec_ntt
+* Name:        neon_polyvec_ntt
 *
 * Description: Apply forward NTT to all elements of a vector of polynomials
 *
@@ -18,13 +17,12 @@ void neon_polyvec_ntt(polyvec *r)
   unsigned int i;
   for (i = 0; i < KYBER_K; i++)
   {
-    neon_ntt(r->vec[i].coeffs);
-    neon_poly_reduce(&r->vec[i]);
+    neon_poly_ntt(&r->vec[i]);
   }
 }
 
 /*************************************************
-* Name:        polyvec_invntt_tomont
+* Name:        neon_polyvec_invntt_to_mont
 *
 * Description: Apply inverse NTT to all elements of a vector of polynomials
 *              and multiply by Montgomery factor 2^16
@@ -35,35 +33,28 @@ void neon_polyvec_invntt_to_mont(polyvec *r)
 {
   unsigned int i;
   for (i = 0; i < KYBER_K; i++)
-    neon_invntt(r->vec[i].coeffs);
+    neon_poly_invntt_tomont(&r->vec[i]);
 }
 
 /*************************************************
-* Name:        polyvec_add
-*
-* Description: Add vectors of polynomials
-*
-* Arguments: - polyvec *r:       pointer to output vector of polynomials
-*            - const polyvec *a: pointer to first input vector of polynomials
-*            - const polyvec *b: pointer to second input vector of polynomials
-**************************************************/
-/*************************************************
-* Name:        polyvec_reduce
+* Name:        neon_polyvec_add_reduce
 *
 * Description: Applies Barrett reduction to each coefficient
 *              of each element of a vector of polynomials
 *              for details of the Barrett reduction see comments in reduce.c
 *
-* Arguments:   - poly *r: pointer to input/output polynomial
+* Arguments: - polyvec *r:       pointer to output vector of polynomials
+*            - const polyvec *a: pointer to first input vector of polynomials
+*            - const polyvec *b: pointer to second input vector of polynomials
 **************************************************/
-void neon_polyvec_add_reduce_csubq(polyvec *c, const polyvec *a)
+void neon_polyvec_add_reduce(polyvec *c, const polyvec *a)
 {
   unsigned int i;
   for (i = 0; i < KYBER_K; i++)
   {
     // c = c + a;
     // c = reduce(c);
-    neon_poly_add_reduce_csubq(&c->vec[i], &a->vec[i]);
+    neon_poly_add_reduce(&c->vec[i], &a->vec[i]);
   }
 }
 
@@ -80,13 +71,33 @@ void neon_polyvec_add_reduce_csubq(polyvec *c, const polyvec *a)
 // c (int16x8) = a - b (int16x8)
 #define vsub8(c, a, b) c = vsubq_s16(a, b);
 
-/* 
+/*************************************************
+* Name:        fqmul
+*
+* Description: Multiplication followed by Montgomery reduction
+*
+* Arguments:   - int16_t a: first factor
+*              - int16_t b: second factor
+*
+* Returns 16-bit integer congruent to a*b*R^{-1} mod q
+
 out, in: int16x8_t
 zeta: input : int16x8_t
 t : int16x8x4_t
 neon_qinv: const   : int16x8_t
 neon_kyberq: const : int16x8_t
-*/
+rewrite pseudo code:
+int16_t fqmul(int16_t b, int16_t c) {
+  int32_t t, u, a;
+
+  a = (int32_t) b*c;
+  (a_L, a_H) = a
+  a_L = a_L * QINV;
+  t = a_L * Q;
+  (t_L, t_H) = t;
+  return t_H - a_H;
+}
+*************************************************/
 #define fqmul(out, in, zeta, t)                                                                              \
   t.val[0] = (int16x8_t)vmull_s16(vget_low_s16(in), vget_low_s16(zeta));                                     \
   t.val[1] = (int16x8_t)vmull_high_s16(in, zeta);                                                            \
@@ -97,6 +108,26 @@ neon_kyberq: const : int16x8_t
   t.val[2] = (int16x8_t)vmull_high_s16(t.val[0], neon_kyberq);                        /* t_H = a_L * Q*/     \
   t.val[0] = vuzp2q_s16(t.val[1], t.val[2]);                                          /* t_H */              \
   out = vsubq_s16(t.val[3], t.val[0]);                                                /* t_H - a_H */
+
+/*
+inout: int16x4_t
+t32 : int32x4_t
+t16: int16x4_t
+neon_v: int16x4_t
+neon_kyberq16: inout int16x4_t
+
+int16_t barrett_reduce(int16_t a) {
+  int16_t t;
+  const int16_t v = ((1U << 26) + KYBER_Q / 2) / KYBER_Q;
+
+  t = (int32_t)v * a;
+  (t_L, t_H) = t; 
+  t_h = t_H + (1 << 9);
+  t_H = t_H >> 10;
+  t_H = a - t_H * KYBER_Q;
+  return t_H;
+}
+*/
 
 /*
 reduce low and high of 
@@ -110,28 +141,30 @@ neon_v, neon_kyber16
   t.val[i] = (int16x8_t)vmull_s16(vget_low_s16(inout), vget_low_s16(neon_v)); \
   t.val[i + 1] = (int16x8_t)vmull_high_s16(inout, neon_v);                    \
   t.val[i] = vuzp2q_s16(t.val[i], t.val[i + 1]);                              \
-  t.val[i + 1] = vshrq_n_s16(t.val[i], 10);                                   \
+  t.val[i + 1] = vaddq_s16(t.val[i], neon_one);                               \
+  t.val[i + 1] = vshrq_n_s16(t.val[i + 1], 10);                               \
   inout = vmlsq_s16(inout, t.val[i + 1], neon_kyberq);
 
 /*************************************************
-* Name:        polyvec_pointwise_acc_montgomery
+* Name:        neon_polyvec_acc_montgomery
 *
-* Description: Pointwise multiply elements of a and b, accumulate into r,
+* Description: Multiply elements of a and b in NTT domain, accumulate into r,
 *              and multiply by 2^-16.
 *
-* Arguments: - poly *r:          pointer to output polynomial
+* Arguments: - poly *r: pointer to output polynomial
 *            - const polyvec *a: pointer to first input vector of polynomials
 *            - const polyvec *b: pointer to second input vector of polynomials
 **************************************************/
 void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, const int to_mont)
 {
-  int16x8x4_t aa, bb, r, ta, tb, t;                    // 24
-  int16x8_t neon_v, neon_qinv, neon_kyberq, neon_zeta; // 4
+  int16x8x4_t aa, bb, r, ta, tb, t;                              // 24
+  int16x8_t neon_v, neon_qinv, neon_kyberq, neon_zeta, neon_one; // 5
 
   // Declare constant
   neon_qinv = vdupq_n_s16(QINV);
   neon_kyberq = vdupq_n_s16(KYBER_Q);
   neon_v = vdupq_n_s16(_V);
+  neon_one = vdupq_n_s16(1 << 9);
 
   // Scalar variable
   unsigned int k = 80;
@@ -155,7 +188,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     vload4(bb, &b->vec[0].coeffs[j]);
 
     // => r.val[0] = a.val[1]*b.val[1]*zeta_pos + a.val[0] * b.val[0]
-    // => r.val[1] = a.val[0]*b.val[1] + a.val[1] * b.val[0] 
+    // => r.val[1] = a.val[0]*b.val[1] + a.val[1] * b.val[0]
     // => r.val[2] = a.val[3]*b.val[3]*zetas_neg + a.val[2]*b.val[2]
     // => r.val[3] = a.val[2]*b.val[3] + a.val[3] * b.val[2]
 
@@ -170,7 +203,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(tb.val[1], aa.val[1], bb.val[0], t);
     fqmul(tb.val[2], aa.val[2], bb.val[2], t);
     fqmul(tb.val[3], aa.val[3], bb.val[2], t);
-    
+
     vadd8(r.val[0], ta.val[0], tb.val[0]);
     vadd8(r.val[1], ta.val[1], tb.val[1]);
     vsub8(r.val[2], tb.val[2], ta.val[2]);
@@ -188,7 +221,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(ta.val[2], aa.val[3], bb.val[3], t);
     fqmul(ta.val[2], ta.val[2], neon_zeta, t);
     fqmul(ta.val[3], aa.val[2], bb.val[3], t);
-    
+
     vadd8(r.val[0], r.val[0], ta.val[0]);
     vadd8(r.val[1], r.val[1], ta.val[1]);
     vsub8(r.val[2], r.val[2], ta.val[2]);
@@ -198,7 +231,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(tb.val[1], aa.val[1], bb.val[0], t);
     fqmul(tb.val[2], aa.val[2], bb.val[2], t);
     fqmul(tb.val[3], aa.val[3], bb.val[2], t);
-    
+
     vadd8(r.val[0], r.val[0], tb.val[0]);
     vadd8(r.val[1], r.val[1], tb.val[1]);
     vadd8(r.val[2], r.val[2], tb.val[2]);
@@ -217,7 +250,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(ta.val[2], aa.val[3], bb.val[3], t);
     fqmul(ta.val[2], ta.val[2], neon_zeta, t);
     fqmul(ta.val[3], aa.val[2], bb.val[3], t);
-    
+
     vadd8(r.val[0], r.val[0], ta.val[0]);
     vadd8(r.val[1], r.val[1], ta.val[1]);
     vsub8(r.val[2], r.val[2], ta.val[2]);
@@ -227,7 +260,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(tb.val[1], aa.val[1], bb.val[0], t);
     fqmul(tb.val[2], aa.val[2], bb.val[2], t);
     fqmul(tb.val[3], aa.val[3], bb.val[2], t);
-    
+
     vadd8(r.val[0], r.val[0], tb.val[0]);
     vadd8(r.val[1], r.val[1], tb.val[1]);
     vadd8(r.val[2], r.val[2], tb.val[2]);
@@ -244,7 +277,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(ta.val[2], aa.val[3], bb.val[3], t);
     fqmul(ta.val[2], ta.val[2], neon_zeta, t);
     fqmul(ta.val[3], aa.val[2], bb.val[3], t);
-    
+
     vadd8(r.val[0], r.val[0], ta.val[0]);
     vadd8(r.val[1], r.val[1], ta.val[1]);
     vsub8(r.val[2], r.val[2], ta.val[2]);
@@ -254,7 +287,7 @@ void neon_polyvec_acc_montgomery(poly *c, const polyvec *a, const polyvec *b, co
     fqmul(tb.val[1], aa.val[1], bb.val[0], t);
     fqmul(tb.val[2], aa.val[2], bb.val[2], t);
     fqmul(tb.val[3], aa.val[3], bb.val[2], t);
-    
+
     vadd8(r.val[0], r.val[0], tb.val[0]);
     vadd8(r.val[1], r.val[1], tb.val[1]);
     vadd8(r.val[2], r.val[2], tb.val[2]);
