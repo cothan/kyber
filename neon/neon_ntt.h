de767f8574fdb2f374ff968d9ca1c6ebd75616bb
cothan <cothannguyen@gmail.com>
2021-02-14 22:58:26 -0500

    refactor neon_ntt.* code

--- neon/neon_ntt.h
+++ neon/neon_ntt.h
@@ -4,16 +4,16 @@
 #include <stdint.h>
 #include "params.h"
 
-#define neon_zetas_inv KYBER_NAMESPACE(_neon_zetas_inv)
+#define neon_zetas_inv KYBER_NAMESPACE(neon_zetas_inv)
 extern const int16_t neon_zetas_inv[272];
 
-#define neon_zetas KYBER_NAMESPACE(_neon_zetas)
+#define neon_zetas KYBER_NAMESPACE(neon_zetas)
 extern const int16_t neon_zetas[224];
 
-#define neon_ntt KYBER_NAMESPACE(_neon_ntt)
+#define neon_ntt KYBER_NAMESPACE(neon_ntt)
 void neon_ntt(int16_t poly[256]);
 
-#define neon_invntt KYBER_NAMESPACE(_neon_invntt)
+#define neon_invntt KYBER_NAMESPACE(neon_invntt)
 void neon_invntt(int16_t poly[256]);
 
 #endif
