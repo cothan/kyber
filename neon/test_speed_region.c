8cfce9cac93e7008c9a541613d734c59c9ea5961
cothan <cothannguyen@gmail.com>
2021-02-26 00:13:51 -0500

    change to clock_gettime

--- neon/test_speed_region.c
+++ neon/test_speed_region.c
@@ -8,18 +8,18 @@
 #include "indcpa.h"
 #include "poly.h"
 #include "polyvec.h"
-// #include "cpucycles.h"
-// #include "speed_print.h"
 #include <time.h>
 #include "print.h"
 #include "neon_ntt.h"
 
-// micro second 
 #define NTESTS 1000000
-// #define NTESTS (1 << 27)
 
-uint64_t t[NTESTS];
 uint8_t seed[KYBER_SYMBYTES] = {0};
+
+#define TIME(s) clock_gettime(CLOCK_MONOTONIC_RAW, &s);
+// Result is nanosecond per call 
+#define  CALC(start, stop) \
+  (double) ((stop.tv_sec - start.tv_sec) * 1000000000 + (stop.tv_nsec - start.tv_nsec)) / NTESTS;
 
 int main()
 {
@@ -34,136 +34,108 @@ int main()
   unsigned char msg[KYBER_INDCPA_MSGBYTES] = {0};
   polyvec matrix[KYBER_K];
   poly ap, bp;
-  clock_t start, end; 
+  struct timespec start, stop;
+  long ns;
 
-  // PAPI_hl_region_begin("gen_matrix");
-  start = clock();
+
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     gen_matrix(matrix, seed, 0);
   }
-  end =  clock() - start;
-  print("gen_matrix:", ((double) end)/CLOCKS_PER_SEC);
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("gen_matrix:", ns);
 
-  // PAPI_hl_region_end("gen_matrix");
 
-  // PAPI_hl_region_begin("neon_poly_getnoise_eta1_2x");
-  start = clock();
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     neon_poly_getnoise_eta1_2x(&ap, &bp, seed, 0, 1);
   }
-  end = clock() - start;
-  print("neon_poly_getnoise_eta1_2x:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("neon_poly_getnoise_eta1_2x");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("neon_poly_getnoise_eta1_2x:", ns);
 
-  // PAPI_hl_region_begin("neon_poly_getnoise_eta2");
-  start = clock();
+
+
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     neon_poly_getnoise_eta2(&ap, seed, 0);
   }
-  end = clock() - start; 
-  print("neon_poly_getnoise_eta2:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("neon_poly_getnoise_eta2");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("neon_poly_getnoise_eta2:", ns);
 
-  // PAPI_hl_region_begin("poly_tomsg");
-  start = clock();
+
+
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     poly_tomsg(msg, &ap);
   }
-  end = clock() - start;
-  print("poly_tomsg:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("poly_tomsg");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("poly_tomsg:", ns);
 
-  // PAPI_hl_region_begin("poly_frommsg");
-  start = clock(); 
+
+
+  TIME(start); 
   for(i=0;i<NTESTS;i++) {
     poly_frommsg(&ap, msg);
   }
-  end = clock() - start;
-  print("poly_frommsg:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("poly_frommsg");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("poly_frommsg:", ns);
 
 
-  // PAPI_hl_region_begin("neon_ntt");
-  start = clock();
+
+
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     neon_ntt(ap.coeffs);
   }
-  end = clock() - start; 
-  print("neon_ntt:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("neon_ntt");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("neon_ntt:", ns);
 
-  // PAPI_hl_region_begin("neon_invntt");
-  start = clock();
+
+
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     neon_invntt(ap.coeffs);
   }
-  end = clock() - start; 
-  print("neon_invntt:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("neon_invntt");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("neon_invntt:", ns);
 
-  // PAPI_hl_region_begin("crypto_kem_keypair");
-  start = clock();
+
+
+  TIME(start);
   for(i=0;i<NTESTS;i++) {
     crypto_kem_keypair(pk, sk);
   }
-  end = clock() - start; 
-  print("crypto_kem_keypair:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("crypto_kem_keypair");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("crypto_kem_keypair:", ns);
 
-  // PAPI_hl_region_begin("crypto_kem_enc");
-  start = clock(); 
+
+
+  TIME(start); 
   for(i=0;i<NTESTS;i++) {
     crypto_kem_enc(ct, key, pk);
   }
-  end = clock() - start; 
-  print("crypto_kem_enc:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("crypto_kem_enc");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("crypto_kem_enc:", ns);
 
-  // PAPI_hl_region_begin("crypto_kem_dec");
-  start = clock(); 
+
+
+  TIME(start); 
   for(i=0;i<NTESTS;i++) {
     crypto_kem_dec(key, ct, sk);
   }
-  end = clock() - start; 
-  print("crypto_kem_enc:", ((double) end)/CLOCKS_PER_SEC);
-  // PAPI_hl_region_end("crypto_kem_dec");
+  TIME(stop);
+  ns = CALC(start, stop);
+  print("crypto_kem_enc:", ns);
 
-  /*
-  PAPI_hl_region_begin("kex_uake_initA");
-  for(i=0;i<NTESTS;i++) {
-    kex_uake_initA(kexsenda, key, sk, pk);
-  }
-  PAPI_hl_region_end("kex_uake_initA");
 
-  PAPI_hl_region_begin("kex_uake_sharedB");
-  for(i=0;i<NTESTS;i++) {
-    kex_uake_sharedB(kexsendb, kexkey, kexsenda, sk);
-  }
-  PAPI_hl_region_end("kex_uake_sharedB");
-
-  PAPI_hl_region_begin("kex_uake_sharedA");
-  for(i=0;i<NTESTS;i++) {
-    kex_uake_sharedA(kexkey, kexsendb, key, sk);
-  }
-  PAPI_hl_region_end("kex_uake_sharedA");
-
-  PAPI_hl_region_begin("kex_ake_initA");
-  for(i=0;i<NTESTS;i++) {
-    kex_ake_initA(kexsenda, key, sk, pk);
-  }
-  PAPI_hl_region_end("kex_ake_initA");
-
-  PAPI_hl_region_begin("kex_ake_sharedB");
-  for(i=0;i<NTESTS;i++) {
-    kex_ake_sharedB(kexsendb, kexkey, kexsenda, sk, pk);
-  }
-  PAPI_hl_region_end("kex_ake_sharedB");
-
-  PAPI_hl_region_begin("kex_ake_sharedA");
-  for(i=0;i<NTESTS;i++) {
-    kex_ake_sharedA(kexkey, kexsendb, key, sk, sk);
-  }
-  PAPI_hl_region_end("kex_ake_sharedA");
-  */
   return 0;
 }
